b6b73367361f15880118eb14d63848a6364f01ce
Denzyl Balram <denzyl.balram@gmail.com>
2015-07-30 10:23:28 -0700

    Login added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def show; end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user_id
+      flash[:notice] = "You are registered."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your profile was updated."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
