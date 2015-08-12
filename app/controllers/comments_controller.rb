6af7a7a8c1cfc77af5a4ad5cabe5b34d1d889367
Denzyl Balram <denzyl.balram@gmail.com>
2015-08-12 11:42:57 -0700

    Set up Comment Votes UI

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,4 +14,11 @@ class CommentsController < ApplicationController
     end
   end
 
+  def vote
+    comment = Comment.find(params[:id])
+    Vote.create(voteable: comment, creator: current_user, vote: params[:vote])
+    flash[:notice] = "Your vote was counted."
+    redirect_to :back
+  end
+
 end
